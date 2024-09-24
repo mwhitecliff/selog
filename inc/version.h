7b9ce8ddcf0286fa6bd4963b6134b13ea1e734c2
Marc von Weissenfluh <m.vw@gmx.ch>
2024-09-24 10:43:48 +0200

    fix header file

--- inc/version.h
+++ inc/version.h
@@ -4,7 +4,11 @@
  * 
  *    SPDX-License-Identifier: Apache-2.0
  */
+#ifndef VERSION_H
+#define VERSION_H
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_FIX 0
+
+#endif /*VERSION_H */
