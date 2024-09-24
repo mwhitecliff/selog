8c8b2af459d4e3f96ad4e4334941adc75cc4e489
Marc von Weissenfluh <m.vw@gmx.ch>
2024-09-24 09:50:56 +0200

    use logging module

--- src/main.c
+++ src/main.c
@@ -1,12 +1,18 @@
 /*
- *    2024 by mwhitecliff
- *    Zephyr Workshop Noser
+ *    (C) 2024 by mwhitecliff
+ *    Zephyr Workshop
+ * 
+ *    SPDX-License-Identifier: Apache-2.0
  */
+
 
 #include <zephyr/kernel.h>
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(selog, LOG_LEVEL_INF);
+
 int main (void)
 {
-    printk("Hello world from %s\n", CONFIG_BOARD);
+    LOG_INF("Hello world");
     return 0;
 }
